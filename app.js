2d350b1be3b0de58d1fa756f13ef29ac32866c19
danivanee <danivanee_170@hotmail.com>
2022-12-12 16:55:21 -0600

    correccion

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
-const possibleChoises = document.querySelectorAll('button')
+const possibleChoices = document.querySelectorAll('button')
 
-possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
-    userChoice = e.target.iduserChoiceDisplay. innerHTML = userChoice
+    userChoiceDisplay.innerHTML = userChoice
 }))
