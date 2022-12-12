9b24f842139e6b125fc2964f20c9121ac3cf42e4
danivanee <danivanee_170@hotmail.com>
2022-12-12 16:51:58 -0600

    funcionalidad al darle click

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const computerChoice = document.getElementById('computer-choice')
 const userChoice = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoises = document.querySelectorAll('button')
+
+possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoice = e.target.iduserChoiceDisplay. innerHTML = userChoice
+}))
