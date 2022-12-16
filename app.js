8018a2c84092c5f74e56a9664d53639db9f11e7f
danivanee <danivanee_170@hotmail.com>
2022-12-15 19:25:10 -0600

    funcionalidad

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
@@ -23,4 +26,10 @@ function generateComputerChoice() {
     computerChoice = 'paper'
    }
    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result 
+    }
 }
